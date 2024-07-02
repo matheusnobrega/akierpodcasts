4e24d32814cd0b03d86e4dfbdfd35a993b6a3e34
matheusnobrega <matheus.nobrega.duarte@gmail.com>
2024-07-02 14:13:31 -0300

    commit inicial

---
+++ src/main/java/com/akier/akierpodcasts/AkierpodcastsApplication.java
@@ -0,0 +1,13 @@
+package com.akier.akierpodcasts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkierpodcastsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkierpodcastsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akier/akierpodcasts/AkierpodcastsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akier.akierpodcasts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkierpodcastsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
